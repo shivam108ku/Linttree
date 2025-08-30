fbb2ceecfd02510e3537ca9c1fdd8a6cfdbf5a88
Shivam Kumar <shivam108ku@gmail.com>
2025-08-30 11:46:22 +0530

    first

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import AboutComponent from '@/components/about'
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <AboutComponent/>
+    </div>
+  )
+}
+
+export default About;

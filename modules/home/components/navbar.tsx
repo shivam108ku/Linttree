f072315853d4f5e866b83bd72e4d0fa5f4169686
Shivam Kumar <shivam108ku@gmail.com>
2025-09-08 16:21:28 +0530

    add layout and navbar components for home page

---
+++ modules/home/components/navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+import { SignInButton , SignUpButton } from '@clerk/nextjs'
+import { SignedIn , SignedOut } from '@clerk/nextjs'
+
+import Link from 'next/link';
+import Image from 'next/image';
+
+const NavBar = () => {
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar

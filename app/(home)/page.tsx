c673c80cac3f4922297d453738e5c8468fa7848a
Shivam Kumar <shivam108ku@gmail.com>
2025-09-08 17:30:49 +0530

    add homePage ui

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { onBoardUser } from "@/modules/auth/actions";
+import  Link  from "next/link";
+import React from "react";
 
-const HomePage = () => {
+const HomePage = async () => {
+  await onBoardUser();
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="min-h-screen">
+      <main className="text-center space-y-8 py-32">
+        <div className="space-y-6">
+          <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold tracking-tight leading-tight text-zinc-700 dark:text-zinc-100">
+            Everything you are.
+            <br />
+            <span className="text-[#41B313]">In one simple link.</span>
+          </h1>
 
-export default HomePage
+          <p className="text-lg md:text-xl text-gray-400 max-w-2xl mx-auto leading-relaxed">
+            Join 70M+ people using TreeBio for their link in bio. One link to
+            help you share everything you create, curate and sell from your
+            social media profiles.
+          </p>
+
+          <div className="pt-4">
+            <Link href="/admin/my-tree">
+                <Button size="lg" className="px-8 py-3 text-lg font-medium cursor-pointer">
+                  TreeBio Dashboard
+                </Button>
+            </Link>
+          </div>
+          
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default HomePage;

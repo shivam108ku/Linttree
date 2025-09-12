0cba55f658d95cd63b5904871e3b4fbd7bcf3813
Shivam Kumar <shivam108ku@gmail.com>
2025-09-12 10:25:50 +0530

    refactor getAvailableUsernameSuggestion function; improve username suggestion logic

--- modules/profile/utils/index.ts
+++ modules/profile/utils/index.ts
@@ -2,4 +2,19 @@ import { db } from "@/lib/db";
 
 export async function getAvailableUsernameSuggestion(base:string , count = 3 , maxTries=10){
     const suggestions:string[] = [];
+
+    for(let i=1; suggestions.length < count && i < maxTries; i++){
+        const candidate = `${base}${i}`
+
+        const exists = await db.user.findUnique({
+            where:{
+                username:candidate
+            }
+        })
+
+        if(!exists){
+            suggestions.push(candidate) 
+        }
+    }
+    return suggestions;
 }
